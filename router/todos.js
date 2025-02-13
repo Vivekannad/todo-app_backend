70fc55216f6678baf528b732e8cf99be7e1396a7
Vivek Anand <dhevikchanintha@gmail.com>
2025-02-13 23:28:18 +0500

    MVC model applied

--- router/todos.js
+++ router/todos.js
@@ -1,61 +1,12 @@
 const router = require("express").Router();
-const modal = require("../models/schema");
-const {v4 : uuidv4} = require("uuid");
+const { getAllTodos, deleteTodo, updateTodo, getOneTodo, createTodo } = require("../controllers/todos");
 
-router.get("/", async(req,res) => {
-    const data = await modal.find();
-    res.status(200).json({status:200 , data});
-})
 
-router.post("/", async(req,res) => {
-    const {todo} = req.body;
-    const found = await modal.findOne({todo: todo});
-    if(!found){
-        await modal.create({
-            todo ,
-            id : uuidv4()
-        });
-        return res.status(201).json({status: 201 , message : "Added successfully"});
-    }
-     res.status(400).json({status : 400 , message : "todo already there"});
-});
+router.route("/").get(getAllTodos).post(createTodo)
 
-router.get("/:id", async(req,res) => {
-    const {id} = req.params;
-    const todo = await modal.findOne({id:id});
-    if(!todo){
-        return res.status(400).json({status:400 , message: "Id Not present!"})
-    }
-    res.status(200).json({status:200 , message: "Accessed successfully", todo})
-})
-
-router.patch("/:id", async(req,res) => {
-    const {id} = req.params;
-    const {todo} = req.body;
-    try{
-        const updated = await modal.findOneAndUpdate(
-            {id : id}, 
-            { $set :  {todo : todo}}, 
-            {new : true}
-        )
-        if(!updated){
-            return res.status(400).json({status : 400 , message : "Wrong Id"});
-        }
-        res.status(200).json({status: 200 , message : "Editted", updated});
-    } catch(err){
-        res.status(400).json({status:400 , message: "wrong id Format"});
-    }
-})
-
-router.delete("/:id", async(req,res) =>{
-    const {id} = req.params;
-    const deleted = await modal.findOneAndDelete(
-        {id : id},
-    )
-    if(!deleted){
-        return res.status(400).json({status:400 , message: "Id Not Present!"});
-    }
-    res.status(200).json({status: 200 , message : "Todo Deleted", deleted});
-})
+router.route("/:id")
+.get(getOneTodo)
+.patch(updateTodo)
+.delete(deleteTodo)
 
 module.exports = router;
